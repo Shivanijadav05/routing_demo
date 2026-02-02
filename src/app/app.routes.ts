17f3f82b6684a2b33d6a8ac0c6eff1df6e63ff55
Shivanijadav05 <shivanihjadav@gmail.com>
2026-02-02 14:57:13 +0530

    initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    {path:'login', component:Login},
+    {path:'contact' ,component:Contact},
+    {path:'about' ,component:About}
+];
